50fd0b76e5c832abfbc36c7417c8aea1e2a5b526
Carlos Fernandez <cfern1990@gmail.com>
2024-03-20 18:11:24 -0400

    Support enter key and fix editing clock value in form

--- module/dialog.js
+++ module/dialog.js
@@ -3,14 +3,19 @@ import { MODULE_ID } from "./settings.js";
 const CLOCK_MAX_SIZE = 32;
 const CLOCK_SIZES = [2, 3, 4, 5, 6, 8, 10, 12];
 
-export class ClockAddDialog extends Application {
+export class ClockAddDialog extends FormApplication {
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
             title: "GlobalProgressClocks.CreateDialog.Title",
             template: "modules/global-progress-clocks/templates/clock-add-dialog.hbs",
             classes: ["dialog"],
             width: 400,
+            submitOnChange: false,
         });
+    }
+
+    get id() {
+        return this.clock ? `${this.clock.id}-edit-global-prog-clock` : `add-global-prog-clock`;
     }
 
     get title() {
@@ -18,13 +23,13 @@ export class ClockAddDialog extends Application {
     }
 
     constructor(clock, complete) {
-        super();
+        super(clock);
         this.clock = clock;
         this.complete = complete;
     }
 
-    async getData() {
-        const data = await super.getData();
+    async getData(options) {
+        const data = await super.getData(options);
         return {
             ...data,
             clock: this.clock,
@@ -37,29 +42,23 @@ export class ClockAddDialog extends Application {
     activateListeners($html) {
         super.activateListeners($html);
 
-        // Autofocus the name. Move to _injectHTML if we need to re-render
-        $html.find("[autofocus]")[0]?.focus();
-
         const inputElement = $html.find(".dropdown-wrapper input");
         $html.find(".dropdown li").on("mousedown", (event) => {
             inputElement.val(event.target.getAttribute("data-value"));
         });
+    }
 
-        $html.find(".dialog-button").on("click", (evt) => {
-            evt.preventDefault();
-            const button = evt.target.dataset.button;
-            if (button === "yes") {
-                const form = $html[0].querySelector("form");
-                const data = new FormDataExtended(form).object;
-                if (this.clock) {
-                    data.id = this.clock.id;
-                    data.value = Math.clamped(this.clock.value, 0, data.max);
-                }
+    _updateObject(event, data) {
+        if (event.submitter.dataset.button !== "yes") {
+            return;
+        }
 
-                this.complete(data);
-            }
+        data.max = Math.max(data.max, 1);
+        if (this.clock) {
+            data.id = this.clock.id;
+            data.value = Math.clamped(data.value, 0, data.max);
+        }
 
-            this.close();
-        });
+        this.complete(data);
     }
 }
